dcd7e05e0bec20d4550e625332f801b65a15bd66
{Takuya <{tkynsmtub@gmail.com}>
2022-04-13 14:27:37 +0900

    4131427

--- test.py
+++ test.py
@@ -2,3 +2,12 @@ import numpy as np
 a=np.array([1,2,3,4,5])
 print(a)
 a=np.append(a,np.array([2,5,8]))
+b=np.array([10,20,30,40,50,60,70])
+c=np.array([4,5,6,7,8,9])
+d=10
+e=print("a")
+
+f=print(a)
+
+
+bcd=10
